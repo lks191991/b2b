543a2114fddd4044f096ac44cb900ce60c18a79c
dslokesh <lokesh.kumar@dotsquares.com>
2025-02-17 12:54:18 +0530

    ssfdSF

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -625,6 +625,43 @@ return view('auth.updatePage', compact('allrecords','notifications','announcment
     }
 	
 	public function tourData()
+    {
+			$url = "https://sandbox.raynatours.com/api/Tour/tourstaticdata"; // API ka URL
+$token = 'eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJhNzAyOTAzZi1jYzA2LTQ5MjktOGNiNS0xN2E1NjJkY2JlNmIiLCJVc2VySWQiOiI1NDg5NiIsIlVzZXJUeXBlIjoiQWdlbnQiLCJQYXJlbnRJRCI6IjAiLCJFbWFpbElEIjoibmlraGlsQGFiYXRlcmFjb25zdWx0aW5nLmNvbSIsImlzcyI6Imh0dHA6Ly9kZXZyYXluYWFwaS5yYXluYXRvdXJzLmNvbS8iLCJhdWQiOiJodHRwOi8vZGV2cmF5bmFhcGkucmF5bmF0b3Vycy5jb20vIn0.4snkHjBlUrdGjQbnMdh3FIZQz8sIQ8Vl_ooSzB5BBKQ';
+$data = [
+    "countryId" => 13063,
+    "cityId" => 13668
+];
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+
+curl_setopt($ch, CURLOPT_HTTPHEADER, [
+    "Content-Type: application/json",
+    "Authorization: Bearer " . trim($token),
+    "Accept: application/json",
+	//"User-Agent: PostmanRuntime/7.29.2"
+    "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+]);
+
+$response = curl_exec($ch);
+$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+curl_close($ch);
+
+$responseArray = json_decode($response, true);
+echo "HTTP Code: CURL" . $httpCode . "\n";
+echo "<pre>";
+print_r($responseArray);
+
+        exit;
+    } 
+	
+	public function tourDataAAA()
 {
     $url = "https://sandbox.raynatours.com/api/Tour/tourstaticdata";
     $token = 'eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJhNzAyOTAzZi1jYzA2LTQ5MjktOGNiNS0xN2E1NjJkY2JlNmIiLCJVc2VySWQiOiI1NDg5NiIsIlVzZXJUeXBlIjoiQWdlbnQiLCJQYXJlbnRJRCI6IjAiLCJFbWFpbElEIjoibmlraGlsQGFiYXRlcmFjb25zdWx0aW5nLmNvbSIsImlzcyI6Imh0dHA6Ly9kZXZyYXluYWFwaS5yYXluYXRvdXJzLmNvbS8iLCJhdWQiOiJodHRwOi8vZGV2cmF5bmFhcGkucmF5bmF0b3Vycy5jb20vIn0.4snkHjBlUrdGjQbnMdh3FIZQz8sIQ8Vl_ooSzB5BBKQ';
