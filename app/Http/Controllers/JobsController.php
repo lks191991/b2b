8c7523350dbd9332519ec86bde0cff175acbc14d
Lokesh <sharmalk1991@gmail.com>
2025-04-23 23:10:01 +0530

    ee

--- app/Http/Controllers/JobsController.php
+++ app/Http/Controllers/JobsController.php
@@ -24,7 +24,7 @@ class JobsController extends Controller
 			ProcessTourData::dispatch();
 			ProcessTourOptionData::dispatch();
 
-			return response()->json(['success' => true, 'message' => 'Jobs dispatched successfully. Data will sync after 5 minutes.']);
+			return response()->json(['success' => true, 'message' => 'Jobs dispatched successfully. Data will sync after 10 minutes.']);
 		} catch (\Exception $e) {
 			Log::error('Error dispatching jobs: ' . $e->getMessage());
 			return response()->json(['success' => false, 'message' => 'Failed to dispatch jobs.']);
