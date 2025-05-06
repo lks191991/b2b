89ba7c72c9dd2c65d0557ebc027e836dfdfe7ed6
Lokesh <sharmalk1991@gmail.com>
2025-05-06 09:34:35 +0530

    dsds

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -14,7 +14,7 @@ class Ticket extends Model
         'ticket_no', 'serial_number', 'valid_from', 'valid_till', 'terms_and_conditions',
         'voucher_id', 'voucher_activity_id', 'ticket_generated', 'ticket_downloaded', 
         'ticket_generated_by', 'ticket_downloaded_by', 'generated_time', 'downloaded_time',
-        'net_cost', 'supplier_ticket', 'isRayna', 'rayna_ticket_details', 'rayna_ticketURL'
+        'net_cost', 'supplier_ticket', 'isRayna', 'rayna_ticket_details', 'rayna_ticketURL', 'noOfAdult', 'noOfchild'
     ];
     
 	public function activity()
